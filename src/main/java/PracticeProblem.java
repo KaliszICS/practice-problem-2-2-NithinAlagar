4f922ecca101e558ed6ca8a04ccf948d7e988811
NithinAlagar <343954178@tdsb.ca>
2026-03-12 00:08:08 +0000

    Nithin.A

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,32 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+	
 
 	}
+
+
+public static boolean isEven(int num) { 
+	boolean bool = num%2 == 0;
+	return bool;
+	}
+public static boolean isOdd(int num1) {
+	boolean bool = num1%2 != 0;
+	return bool;
+	}
+
+public static boolean isPositive(int num) { 
+	return 0 < num;
+
+	
+	}
+public static boolean isNegative(int num) { 
+	return 0 > num;
+}
+
+public static int combinedLength(String a, String b) { 
+	return a.length() + b.length();
+}	
+
 
 }
